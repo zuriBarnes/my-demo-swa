1a4c76fac379ec61ea4760254e4ce2d9e4ecae22
mistyb79 <45982373+zuriBarnes@users.noreply.github.com>
2022-02-24 17:45:18 -0500

    error fix in testing

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello from Planet-ZKB</p>
+        <p>Hello from Zuri Barnes</p>
         <a
           className="App-link"
           href="https://reactjs.org"
